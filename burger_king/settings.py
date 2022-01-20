27a26e70d86a4cc5760f6206b1c176058d44ee0b
Ulan Chokushov <ulan.chokushov@gmail.com>
2022-01-20 17:07:24 +0600

    CRUD, cart.

--- burger_king/settings.py
+++ burger_king/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # my libs
+    'cart',
+
     # my apps
     'menu',
+
+
 ]
 
 MIDDLEWARE = [
@@ -68,11 +73,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processor.cart_total_amount',
             ],
         },
     },
 ]
 
+CART_SESSION_ID = 'cart'
 WSGI_APPLICATION = 'burger_king.wsgi.application'
 
 
